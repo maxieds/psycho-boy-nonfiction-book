983a2598669ba364eb285caedb49b374a10769d4
Dr. Maxie Dion Schmidt (Code Goddess LL^HP^HC^H^H) <maxieds@gmail.com>
2026-09-17 13:37:35 -0500

    Archiving saved versions of ARPANet hacks from over two months ago - Continued week after interesting weekend events post 09/11/2026 in Hyde Park, Chicago, Illinois, USA, North America, Earth - Day 4 - Y-6

---
+++ arpanet_restore_from_last/MacOSBook-maxieds/psycho-boy-nonfiction-book/docs/capture-the-flag-maralago-wargames/porche911/misc-docs/robin stern trump i wouldn't even let you suck my dick pins her down and rapes her - I wouldn't even let you suck my dick - McDonald Provosty - Google Search_files/m=sy7w3,SC7lYd,vRlMvf,sy1v7,sy1v6,sy1ua,sy1u6,sy1v5,i8S0_xZNF.js
@@ -0,0 +1,24 @@
+loaded_h_0(function(_){var window=this;
+_.oIr=_.y("SC7lYd",[]);
+_.v("SC7lYd");
+var U2K=function(a,b){var c=!1;b.addEventListener("mousedown",()=>{c=!0});b.addEventListener("mouseup",()=>{(0,_.po)(()=>{c&&a.Ja("UWckNb").click();c=!1},500)});b.addEventListener("dblclick",()=>{c=!1});document.addEventListener("selectionchange",()=>{c&&window.getSelection()&&window.getSelection().toString().length>0&&(c=!1)})},V2K=class extends _.Cs{constructor(a){super(a.Na);this.Rz=null;this.p8();a=this.Ya("VNxLed");a!=null&&this.getData("eflcs").Nb()&&U2K(this,a.hb())}oa(){this.notify("W4ub6d")}Ba(){this.notify("rId5bd")}Aa(){this.notify("Tf6OIc")}p8(){}};
+V2K.prototype.$wa$npAYtf=function(){return this.p8};V2K.prototype.$wa$A6j43c=function(){return this.Aa};V2K.prototype.$wa$qsYrDe=function(){return this.Ba};V2K.prototype.$wa$OMITjf=function(){return this.oa};_.Es(_.oIr,V2K);
+_.x();
+_.v("vRlMvf");
+_.VRd=new _.Yf(_.a3a);
+_.x();
+_.rUd=_.y("i8S0p",[_.Gr,_.a3a]);
+_.mUd=!!(_.Ki[0]&4096);_.nUd=!!(_.Ki[0]&8192);_.oUd=!!(_.Ki[0]>>14&1);_.pUd=!!(_.Ki[0]>>15&1);_.qUd=!!(_.Ki[0]>>16&1);
+_.YTd=[-18,_.XTd,_.F,-1,3,_.F,-3,_.D,_.F,_.Jl,_.I,-2,_.WTd,_.TTd];
+var gUd=[0,_.F,_.D,_.F,-2,_.I,_.F,1,_.F,-5,1,_.F];var hUd=[0,_.G,[0,3,_.I,[-9,{},[0,_.F,-2,_.YTd],[0,_.D,_.F,_.I,_.F,-1,[0,_.F,-2,_.C,-1,_.F,_.C,-1]],[0,_.F,-1,_.D,_.BBb,_.F,-1],[0,_.F,-1],1,gUd,_.$Td,[0,_.C]]],2,[0,_.I,_.F]];var iUd=[0,2,_.F,3,_.Ro,_.F,-1,_.lu,_.I,-1,_.F];var jUd=[0,1,_.D,_.dUd,_.NRd,2,gUd,_.fUd,_.I,_.$Td,_.eUd,_.bUd];var kUd=[0,_.F,-4];_.QG=class extends _.m{constructor(a){super(a,34)}tJ(){return _.p(this,_.aUd,31)}};_.QG.prototype.ub="l7Bhpb";_.SG=[-34,{},14,_.D,2,_.F,_.D,1,hUd,1,iUd,7,_.bUd,kUd,jUd];
+_.lUd=class extends _.m{constructor(a){super(a)}fG(){return _.p(this,_.cUd,8)}tJ(){return _.p(this,_.aUd,12)}};
+_.v("i8S0p");
+var MUd=function(a){var b=new _.qG;var c=_.Zi(a,JUd,32)?1:_.Zi(a,_.lUd,33)?3:2;var d=new _.wUd;c=_.Yh(d,7,c);c=_.Zh(c,4,!0);if(_.Zi(a,JUd,32)){var e,f;d=_.u(a,18);d=_.fh(c,1,d);var g=(e=_.p(a,JUd,32))==null?void 0:e.Es();e=_.fh(d,2,g);d=(f=_.p(a,JUd,32))==null?void 0:_.u(f,4);_.fh(e,3,d);f=_.p(a,JUd,32);_.rh(f,5)&&(f=_.p(a,JUd,32),f=_.u(f,5),_.fh(c,6,f))}b=_.nJd(b,KUd(_.xUd,c));_.Zi(a,JUd,32)&&(c=(new _.yUd).setTitle(_.p(a,JUd,32).getTitle()),f=_.u(a,18),c=_.fh(c,2,f),f=_.p(a,JUd,32).Zj(),c=_.fh(c,
+3,f),_.nJd(b,KUd(_.zUd,c)));if(_.Zi(a,_.lUd,33)){var h,k,l,n,q;c=(new _.DUd).setTitle((h=_.p(a,JUd,32))==null?void 0:h.getTitle());h=(k=_.p(a,_.lUd,33))==null?void 0:_.p(k,_.MRd,4);k=_.Ib(c,_.MRd,3,h);h=(l=_.p(a,_.lUd,33))==null?void 0:_.p(l,_.BUd,3);l=_.Ib(k,_.BUd,10,h);k=(n=_.p(a,_.lUd,33))==null?void 0:n.fG();n=_.Ib(l,_.cUd,14,k);l=(q=_.p(a,_.lUd,33))==null?void 0:_.p(q,_.CUd,11);q=_.Ib(n,_.CUd,16,l);let Z;switch((Z=_.p(a,_.lUd,33))==null?void 0:_.Cj(Z,9)){case 2:var r,t=(r=_.p(a,_.lUd,33))==null?
+void 0:r.tJ();_.Ib(q,_.aUd,12,t);break;case 3:r=(t=_.p(a,_.lUd,33))==null?void 0:_.p(t,_.PG,10),_.Ib(q,_.PG,15,r)}_.nJd(b,KUd(_.EUd,q))}var A,E,H,N,P,S;r=new _.FUd;t=(E=_.p(a,LUd,23))==null?void 0:_.p(E,_.kh,7);E=_.Ib(r,_.kh,2,t);r=(H=_.p(a,LUd,23))==null?void 0:_.Cj(H,12);H=_.Yh(E,15,r);E=(N=_.p(a,LUd,23))==null?void 0:_.u(N,9);N=_.fh(H,11,E);H=(P=_.p(a,LUd,23))==null?void 0:_.Cj(P,11);P=_.Yh(N,14,H);N=(S=_.p(a,LUd,23))==null?void 0:_.u(S,8);S=_.fh(P,3,N);P=_.Zi(a,_.lUd,33);S=_.Zh(S,12,P);P=_.Zi(a,
+JUd,32);S=_.Zh(S,13,P);a=(A=_.p(a,LUd,23))==null?void 0:_.u(A,13);A=_.fh(S,17,a);_.nJd(b,KUd(_.GUd,A));return KUd(_.vUd,new _.uUd).Ca(b)},KUd=function(a,b){return _.oJd(new _.pG,(new _.lG).oa(a,b))},LUd=class extends _.m{constructor(a){super(a)}nL(){return _.u(this,3)}},JUd=class extends _.m{constructor(a){super(a)}getTitle(){return _.u(this,1)}setTitle(a){return _.fh(this,1,a)}wd(){return _.rh(this,1)}Zj(){return _.u(this,2)}Es(){return _.u(this,3)}LO(){return _.rh(this,3)}},NUd=function(a,b){return _.Ib(a,
+_.pG,1,b)},OUd=function(a,b){return _.fh(a,2,b)};var PUd=class extends _.Cs{static Ta(){return{jsdata:{i6c:_.QG},service:{Ch:_.LG,sxc:_.VRd}}}constructor(a){super(a.Na);this.triggerElement=null;this.i6c=a.jsdata.i6c;this.Ch=a.service.Ch;this.sxc=a.service.sxc}async Rc(a){var b=(0,_.Vf)(),c=b();b=b(1);try{a.event&&a.event.preventDefault();let d=a.rb.el();this.triggerElement=d.querySelector('[role="button"]')||d;let e=a.rb.el(),f=OUd(NUd(new _.tUd,MUd(this.i6c)),this.getRoot().el().id),g=this.sxc.getInstance(f,()=>{this.triggerElement&&(this.triggerElement.focus({preventScroll:!0}),
+this.triggerElement=null)});b(await c(this.Ch.showViewer({viewerType:47,entrypoint:e,sB:g,u8:!0,oX:_.qUd?_.IUd:void 0})))}finally{c()}}};PUd.prototype.$wa$h5M12e=function(){return this.Rc};_.Es(_.rUd,PUd);
+_.x();
+});
+// Google Inc.
